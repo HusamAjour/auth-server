dde9001b6d55de9dbc5f7dd640a4e6d1f27ccf6d
HusamAjour <husamaj278@gmail.com>
2021-01-12 20:58:44 +0200

    add class 13 solution

--- src/auth/model/users-model.js
+++ src/auth/model/users-model.js
@@ -22,6 +22,13 @@ class Users {
     let obj = cred ? { username: cred.username } : {};
     return this.schema.find(obj);
   }
+  getById(_id) {
+    if (_id) {
+      return this.schema.findById(_id);
+    } else {
+      return null;
+    }
+  }
   async authenticate(cred) {
     let user = await this.schema.findOne({ username: cred.username }).exec();
     console.log(user);
@@ -45,7 +52,11 @@ class Users {
     );
     return token;
   }
-
+  async checkUser(usr) {
+    let result = await this.get({ username: usr });
+    console.log(result);
+    return result;
+  }
   async checkUsername(usr) {
     console.log(usr);
     let user = await this.schema.findOne({ username: usr }).exec();
@@ -61,71 +72,15 @@ class Users {
       return p;
     }
   }
-}
 
-module.exports = new Users();
+  async authenticateToken(token, secret) {
+    console.log(secret);
+    console.log(token);
+    let tokenObject = await jwt.verify(token, secret);
+    console.log('tokenObject : ', tokenObject);
+    //let checkUserExist = await this.get({ username: tokenObject.username });
 
-/*'use strict';
-
-let usersSchema = require('./users.schema');
-let bcrypt = require('bcrypt');
-let jwt = require('jsonwebtoken');
-class Users {
-  constructor() {
-    this.schema = usersSchema;
-  }
-  create(record) {
-    console.log('inside create user');
-    let newRecord = new this.schema({
-      username: record.username,
-      password: bcrypt.hash(record.password, 10),
-    });
-    return newRecord.save();
-  }
-  get(cred) {
-    let obj = cred ? { username: cred.username } : {};
-    return this.schema.find(obj);
-  }
-  authenticate(cred) {
-    let user = this.schema.findOne({ username: cred.username }).exec();
-    console.log(user);
-    if (user) {
-      let auth = bcrypt.compare(cred.password, user.password);
-      if (auth) {
-        return user;
-      } else {
-        return false;
-      }
-    } else {
-      return null;
-    }
-  }
-  generateAToken(cred) {
-    console.log('Inside generateToken');
-    console.log(cred);
-    let token = jwt.sign(
-      { username: cred.username, password: cred.password },
-      process.env.SECRET
-    );
-    return token;
-  }
-
-  async checkUsername(usr) {
-    console.log(usr);
-    let user = await this.schema.findOne({ username: usr }).exec();
-    if (user) {
-      return user;
-    } else {
-      let pass = 'ouathPass123';
-      let p = await this.create({
-        username: usr,
-        password: pass,
-      });
-      console.log(p);
-      return p;
-    }
+    return tokenObject;
   }
 }
-
 module.exports = new Users();
-*/
--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config;
 const router = require('./auth/router');
+const extraRouter = require('./extra-routes');
 const notFoundHandler = require('./middleware/404');
 const errorHandler = require('./middleware/500');
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => res.status(200).send('Server was hit successflly!'));
 app.use(router);
+app.use(extraRouter);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
